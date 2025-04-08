57e42cb4c4a1b496d8cb889e05c4b4229c525b50
HARSHITA BHARDWAJ <83810229+harshita-2109@users.noreply.github.com>
2025-04-08 21:28:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,10 @@ def get_vectorstore():
     pdf_name = "./BMSL.pdf"
     loaders = [PyPDFLoader(pdf_name)]
     index = VectorstoreIndexCreator(
-        embedding=HuggingFaceEmbeddings(model_name='all-MiniLM-L12-v2'),
+        embedding=HuggingFaceEmbeddings(
+        model_name='all-MiniLM-L12-v2',
+        model_kwargs={"device": "cpu"}  
+        )
         text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     ).from_loaders(loaders)
     return index.vectorstore
